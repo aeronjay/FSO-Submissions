9a6905efb5fe99d2d792a5ce6b8ebfb148925969
TeBats <bulataoaeronjay@gmail.com>
2024-08-24 06:44:05 +0800

    added template

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -1,10 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReacDom from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReacDom.createRoot(document.getElementById('root')).render(<App />)
