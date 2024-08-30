60c8e2a7f824ecfc2f87ef353b8fb99ea611c146
TeBats <bulataoaeronjay@gmail.com>
2024-08-30 15:52:24 +0800

    refactor Content

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -36,9 +36,9 @@ const Content = (props) => {
 
   return(
     <>
-      <Part part = {props.parts[0].name} exercise = {props.parts[0].name}/>
-      <Part part = {props.parts[1].name} exercise = {props.parts[1].name}/>
-      <Part part = {props.parts[2].name} exercise = {props.parts[2].name}/>
+      <Part part = {props.parts[0].name} exercise = {props.parts[0].exercise}/>
+      <Part part = {props.parts[1].name} exercise = {props.parts[1].exercise}/>
+      <Part part = {props.parts[2].name} exercise = {props.parts[2].exercise}/>
     </>
   )
   
