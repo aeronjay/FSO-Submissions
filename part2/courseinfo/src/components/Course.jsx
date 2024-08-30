d345c6a6cea8614f58e50b8e52c62a654d05446d
TeBats <bulataoaeronjay@gmail.com>
2024-08-30 15:59:35 +0800

    add Total course exercises

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -5,12 +5,14 @@ const Course = ({course}) => {
         <>
             <Header headerName={course.name} id={course.id} />
             <Content course={course} />
+            <Total parts={course.parts} />
         </>
         
     )
 }
-const Header = ({headerName, id}) => <h1 id={id}>{headerName}</h1>
+const Header = ({headerName, id}) => <h1 key={id}>{headerName}</h1>
 const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+const Total = ({parts}) => <p><strong>total of {parts.reduce((total, element) => total += element.exercises, 0)}  exercises</strong></p>
 
 const Content = ({course}) => {
     const parts = course.parts
