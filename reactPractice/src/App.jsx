49b4028a5a1d55c446643773bc7ab97d2223efcd
TeBats <bulataoaeronjay@gmail.com>
2024-08-30 13:35:18 +0800

    part2 practice party

--- reactPractice/src/App.jsx
+++ reactPractice/src/App.jsx
@@ -1,60 +1,22 @@
-import { useState } from "react"
+import Note from './components/Note'
 
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
+const App = (props) => {
+  const { notes } = props
 
-  const handleRightClicks = () => {
-    const newRight = clicks.right + 1;
-    const newClicks = {
-      ...clicks,
-      right: newRight
-    }
-    setClicks(newClicks);
-    setAll(allClicks.concat('R'));
-    setTotal(clicks.left + newRight);
-  }
-  const handleLeftClicks = () => {
-    const newLeft = clicks.left + 1;
-    const newClicks = {
-      ...clicks,
-      left: newLeft
-    }
-    setClicks(newClicks);
-    setAll(allClicks.concat('L'));
-    setTotal(newLeft + clicks.right);
-
-  }
-  
-  return (
-    <>
-      {clicks.left}
-      <Button handleClick={handleLeftClicks} name={"left"}/> 
-      <Button handleClick={handleRightClicks} name={"right"}/> 
-      {clicks.right}
-      <History allClicks={allClicks}/>
-      <p>total {total} </p>
-    </>
-  ) 
-}
-const Button = ({handleClick, name}) => <button onClick={handleClick}>{name} </button>
-const History = (props) => {
-  if(props.allClicks.length === 0){
-    return (
-      <div>
-          This App is used by pressing them buttonsz
-      </div>
-    )
-  }
   return (
     <div>
-      {props.allClicks.join(" ")}
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map((note) => 
+            <Note  key={note.id} note={note} />
+          )
+        }
+      </ul>
     </div>
   )
 }
+
+
 
 export default App
