5f91fad7d31f3345858816fc0a2ef287add5771c
TeBats <bulataoaeronjay@gmail.com>
2024-10-18 16:45:53 +0800

    FSO exercise 4.2

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,43 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-require('dotenv').config()
-const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("sueccessfully connected to db"))
-    .catch((error) => console.log("error with database", error))
-    
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server Is running on PORT ${config.PORT}`)
